705540dbf8dc324e98ebbca15e6a2a5cc27993e7
padtrack <iusetrackpad@gmail.com>
2020-06-13 18:45:49 -0700

    Changed DB setup

--- track/scripts/setup.py
+++ track/scripts/setup.py
@@ -6,7 +6,7 @@ with sqlite3.connect('../assets/private/bot.db') as conn:
                                                        builds_channel INTEGER,
                                                        disabled_commands BLOB,
                                                        disabled_cogs BLOB)''')
-    conn.execute('''CREATE TABLE IF NOT EXISTS builds (id INTEGER PRIMARY KEY,
+    conn.execute('''CREATE TABLE IF NOT EXISTS builds (id INTEGER,
                                                        author INTEGER,
                                                        title TEXT,
                                                        description TEXT,
@@ -16,7 +16,6 @@ with sqlite3.connect('../assets/private/bot.db') as conn:
                                                        in_queue INTEGER DEFAULT 0,
                                                        FOREIGN KEY(guild_id) REFERENCES guilds(id))''')
     conn.execute('''CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY,
-                                                      contours_played INTEGER,
-                                                      contours_record REAL)''')
+                                                      data BLOB)''')
     conn.execute('''CREATE TABLE IF NOT EXISTS stats (timestamp INTEGER PRIMARY KEY,
                                                       stats BLOB)''')
